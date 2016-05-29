228f6b5e83c40645e2469aeb2dea070d14d69797
Danilo de Souza <danilodesousa@gmail.com>
2016-05-29 01:27:32 -0300

    Separando as rotas node

--- config/express.js
+++ config/express.js
@@ -1,7 +1,6 @@
 var app = require('express')();
 app.set('view engine','ejs');
-
+app.set('views','./app/views');
 module.exports = function() {
-  console.log("chamando o módulo export express");
   return app;
 }
