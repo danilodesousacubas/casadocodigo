117fa5b2ce65bea6361445a5a6a4d208cdf74b5f
danilodesousacubas <danilodesousa@gmail.comcd>
2016-08-11 20:52:36 -0300

    Criado rota para salvar produto

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,14 +1,29 @@
 module.exports = function(app){
 
-  app.get('/produtos', function(req, res){
+  var listaProdutos = function(req, res){
+
     var connection = app.infra.connectionFactory();
 
-    var produtosBanco = new  app.infra.produtosBanco(connection);
-    produtosBanco.lista(function(err, results){
-         res.render('produtos/lista', {lista:results});
+    var produtosDAO = new  app.infra.produtosDAO(connection);
+    produtosDAO.lista(function(err, results){
+      res.render('produtos/lista', {lista:results});
     });
     connection.end();
-     console.log('Logging');
+  };
+
+  app.get('/produtos', listaProdutos);
+  app.get('/produtos/form', function(req, res){
+    res.render('produtos/form');
   });
 
+  app.post('/produtos', function(req,res){
+
+    var produto = req.body;
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.produtosDAO(connection);
+
+    produtosDAO.salva(produto, function(erros, results){
+      listaProdutos(req,res);
+    });
+  });
 }
